f50c87a020125690f779f92fd1c497388967acf9
KhangCaChua <149504554+Flower-unfurl@users.noreply.github.com>
2026-07-08 01:56:23 +0700

    Delete .next/dev directory

--- .next/dev/static/chunks/src_app_page_tsx_1r2tbvd._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_0xn0s3l._.js","static/chunks/node_modules_0-nppkw._.js"],
-    source: "dynamic"
-});
